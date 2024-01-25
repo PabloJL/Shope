768cdc523262fff02629252c32bc491a07ae0d34
PabloJL <jup_jim_7@hotmail.com>
2024-01-24 19:48:15 -0600

    Api llamada y items estilizados

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,25 +1,28 @@
 // import React from 'react'
+import { IoMdAdd } from "react-icons/io";
 
-function Card() {
+function Card(data) {
   return (
-    <div className="cursor-pointer w-56 h-60 rounded-md  bg-bk drop-shadow-lg">
-      <figure className="relative mb-2 w-full h-4/5">
-        <span className="absolute bottom-0 left-0  bg-white/60 rounded-lg text-black text-xs px-3 py-0.5 m-2">
-          Shoes
+    <div className="cursor-pointer w-56 rounded-md  bg-bk drop-shadow-xl h-80">
+      <figure className="relative mb-2 w-full h-3/5">
+        <span className="absolute bottom-0 left-0 rounded-lg text-black text-xs px-3 py-0.5 m-2 bg-bk/60">
+          {data.data.category.name}
         </span>
         <img
-          src="https://urbanutility.mx/cdn/shop/files/CatalogoEdit2-2023-10-12T132329.448.png?v=1697236182"
-          alt="product"
-          className="w-full h-full object-cover rounded-lg"
+          src={data.data.images[0]}
+          alt={data.data.title}
+          className="w-full h-full rounded-lg object-cover"
+          // eslint-disable-next-line react/no-unknown-property
+          referrerPolicy="no-referrer"
         />
         <button className="absolute top-0 right-0 flex justify-center items-center w-6 h-6 rounded-full m-2 p-0.5 bg-bk">
-          +
+          <IoMdAdd />
         </button>
       </figure>
-      <p className="flex justify-between  px-3">
-        <span className="text-lg font-light">Yeezy Slides</span>
-        <span className="text-lg font-bold">$300</span>
-      </p>
+      <div className="flex flex-col p-5 mt-0  h-2/5 justify-between">
+        <p className="text-sm font-normal">{data.data.title}</p>
+        <p className="text-lg font-bold self-end">${data.data.price}</p>
+      </div>
     </div>
   );
 }
--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,12 +1,33 @@
 // import React from "react";
 import Layout from "../../Components/Layout";
 import Card from "../../Components/Card";
+import { useState, useEffect } from "react";
+import { apiUrl } from "../../Api";
 
 function Home() {
+  const [items, setItems] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${apiUrl}`);
+        const data = await response.json();
+        setItems(data);
+      } catch (error) {
+        console.error(`Ha ocurrido un problema: ${error}`);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
     <Layout>
       Home
-      <Card />
+      <section className="grid place-items-center gap-4 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 w-full max-w-screen-xl">
+        {items?.map((item) => (
+          <Card key={item.id} data={item} />
+        ))}
+      </section>
     </Layout>
   );
 }
